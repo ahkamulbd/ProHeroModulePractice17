31416dd3e390cce1fe34fffb37fb950a640d8769
ahkamulbd <ahkamulbd@gmail.com>
2022-08-01 16:01:03 +0600

    push & pop method in JS addressed according to Lecute 17-4 and shift() & unshift() methods too

---
+++ push-pop.js
@@ -0,0 +1,47 @@
+// Using push to add elements at the end of Array:
+//     // With Numbers:
+// var numbers = [78, 45, 98, 45];
+// console.log(numbers);
+
+// numbers.push(63);
+// console.log(numbers);
+
+// With String
+
+// var friends = ['balam', 'kalam', 'salam'];
+// console.log(friends);
+
+// friends.push('zalam');
+// console.log(friends);
+
+// Using pop to get last element
+
+// var numbers = [78, 45, 98, 45];
+// // console.log(numbers);
+
+// // numbers.pop();
+// // console.log(numbers);
+
+// var elements = numbers.pop();
+
+// console.log(numbers);
+// console.log(elements);
+
+// To remove first element from Array :
+
+// var numbers = [78, 45, 98, 45];
+
+// numbers.shift();
+// console.log(numbers);
+
+// To add element at first position in array in JS:
+
+var numbers = [78, 45, 98, 45];
+console.log(numbers);
+
+numbers.unshift(38);
+
+console.log(numbers);
+
+
+
