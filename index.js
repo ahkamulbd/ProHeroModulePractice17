1460d587db100546bc97aae7ff2829d9f91a489a
ahkamulbd <ahkamulbd@gmail.com>
2022-08-01 15:04:00 +0600

    Index related issues addressed according to Lecture 17-3

---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+//console.log(numbers[3]);
+//console.log(numbers[1]);
+
+// var element = numbers[1];
+// console.log(element);
+
+// numbers[1] = 77;
+// console.log(numbers);
+
+// Find the index of an element:
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
