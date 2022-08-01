9cf811bf6757eb859ac3d668d4cf9661a216d6a8
ahkamulbd <ahkamulbd@gmail.com>
2022-08-01 11:37:18 +0600

    New repo created to explore the fundamental concepts of Array and Conditionals in JS

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 21, 45, 14, 105, 6];  // Number type array
+var picnicFee = [5000, 2000, 4000, 150];  // Number type array
+var nayikas = ['mahi', 'opu', 'sabnoor', 'sabana'];  // String type array
+var vowels = ['a', 'e', 'i', 'o', 'u'];  // Char type array
+
+console.log(vowels);
+console.log(vowels.length);
