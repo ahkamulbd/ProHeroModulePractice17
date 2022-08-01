6207af2b1d44ce83d597b7eb3f1ce955a316ee5c
ahkamulbd <ahkamulbd@gmail.com>
2022-08-01 17:51:32 +0600

    Multiple conditions 'And' & 'Or' addressed according to Lecture 17-7

---
+++ multiple-condition.js
@@ -0,0 +1,17 @@
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+
+// if (isGraduated == true && salary == 50000 && cars >= 1) {
+//     console.log('Let us merry');
+// }
+// else {
+//     console.log('Merry is not in your fate !!!!!!')
+// }
+
+if (isGraduated === true || salary === 50000 || cars >= 1) {
+    console.log('Let us merry');
+}
+else {
+    console.log('Merry is not in your fate !!!!!!')
+}
