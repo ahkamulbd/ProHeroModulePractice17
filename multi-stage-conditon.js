f45f68aa933a7d4904087b6842cb621d5f7ad698
ahkamulbd <ahkamulbd@gmail.com>
2022-08-01 18:15:52 +0600

    Multi stage condition in JS addressed according to Lecture 17-8

---
+++ multi-stage-conditon.js
@@ -0,0 +1,34 @@
+// var money = 15;
+// var danishPrice = 45;
+// var butterBread = 35;
+// var toastBiscuit = 20;
+
+// if (danishPrice < money) {
+//     console.log('Danish Khamu');
+// }
+// else if (butterBread < money) {
+//     console.log('Butter Bread Khamu');
+// }
+// else if (toastBiscuit < money) {
+//     console.log('Toast Biscuit Khamu')
+// }
+// else {
+//     console.log('Shudu Chai Khamu');
+// }
+
+// Nested Condition: 
+
+var math = true;
+var geometry = true;
+
+if (math == true) {
+    if (geometry == true) {
+        console.log('You can be Pithogorus');
+    }
+    else {
+        console.log('You can not be Pithogorus');
+    }
+}
+else {
+    console.log('Learn Math more & more.')
+}
